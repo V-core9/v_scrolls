5d5c1db11a4cc6d77a7909dd29cf7ae11dcb3552
MyUserNameIsMyUserName <slavko.vuletic92@gmail.com>
2021-07-25 08:30:41 +0200

    more added to it

--- system/actions/generate_documentation.js
+++ system/actions/generate_documentation.js
@@ -1,3 +1,17 @@
 const files_export_list = require('../config/docs_export')
 
-console.log("FILES FOR EXPORTING: "+ files_export_list)
+console.log("FILES FOR EXPORTING: ")
+
+var file_content = ""
+
+console.log(files_export_list)
+
+files_export_list.forEach(fileDoc => {
+    console.log(fileDoc)
+    fileDoc.layout.forEach(section =>  {
+        file_content += "Section Title: \n"+ section.title +" \n\n"
+        file_content += "Section Description: \n"+ section.content +" \n\n"
+    })
+});
+
+console.log(file_content)
---
+++ system/info_pages/readme/development.js
@@ -0,0 +1,7 @@
+const development = {
+    title : "Project Folders Structure Info",
+    content: `Made to help out hunting ghost bugs in front and backend.
+                Currently in state of organization of files and building up the idea what else to include`
+}
+
+module.exports = development
