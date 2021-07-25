a5de4b35d451725221372fdd248399223db1ebee
MyUserNameIsMyUserName <slavko.vuletic92@gmail.com>
2021-07-26 00:17:09 +0200

    yup just updated minor things and renaming

--- system/readme_files/readme/install_process.js
+++ system/readme_files/readme/install_process.js
@@ -3,9 +3,12 @@ const install_process = {
     content: `Hopefully very short install process. Build mostly requires
 you to run a single command which will do the rest.
     
-    npm run _setup    
+    npm run _setup_    
         
-After this is complete you should be able to run this.`
+After this is complete you should be able to run this.  
+##  
+> **NOTE**: Server, CLI_Tool & Extension should be installed and set.
+#  `
 }
 
 module.exports = install_process
